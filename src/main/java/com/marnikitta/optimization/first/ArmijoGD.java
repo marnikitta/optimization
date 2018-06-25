7b8d9cb424218a12aa14c9492c43f5ebdb39a940
Nikita Marshalkin <marnikitta@gmail.com>
2018-06-25 16:54:47 +0300

    Hardcode default eta, fix steps

--- src/main/java/com/marnikitta/optimization/first/ArmijoGD.java
+++ src/main/java/com/marnikitta/optimization/first/ArmijoGD.java
@@ -9,7 +9,6 @@ public class ArmijoGD implements FirstOrderMinimizer {
   public static final int DEFAULT_MAX_ORACLE_CALLS = (int) 1.0e8;
   public static final double DEFAULT_TOLERANCE = 1.0e-6;
   public static final double DEFAULT_C1 = 1.0e-6;
-  public static final int DEFAULT_ETA = 2;
 
   private final Logger log = LoggerFactory.getLogger(ArmijoGD.class);
 
@@ -17,18 +16,16 @@ public class ArmijoGD implements FirstOrderMinimizer {
   private final int maxOracleCalls;
   private final double tolerance;
   private final double c1;
-  private final double eta;
 
   public ArmijoGD() {
-    this(DEFAULT_MAX_ITER, DEFAULT_MAX_ORACLE_CALLS, DEFAULT_TOLERANCE, DEFAULT_C1, DEFAULT_ETA);
+    this(DEFAULT_MAX_ITER, DEFAULT_MAX_ORACLE_CALLS, DEFAULT_TOLERANCE, DEFAULT_C1);
   }
 
-  public ArmijoGD(int maxIterations, int maxOracleCalls, double tolerance, double c1, double eta) {
+  public ArmijoGD(int maxIterations, int maxOracleCalls, double tolerance, double c1) {
     this.maxIterations = maxIterations;
     this.maxOracleCalls = maxOracleCalls;
     this.tolerance = tolerance;
     this.c1 = c1;
-    this.eta = eta;
   }
 
   @Override
@@ -37,7 +34,6 @@ public class ArmijoGD implements FirstOrderMinimizer {
     final Vector grad = new Vector(start.length());
 
     Vector armijoAttempt = new Vector(start.length());
-    Vector x = start;
 
     final String header = String.format("%15s %15s %15s %15s\n", "iteration", "grad norm", "fxk", "oracleCalls");
     log.debug(header);
@@ -48,8 +44,8 @@ public class ArmijoGD implements FirstOrderMinimizer {
     double alpha = 1;
 
     while (iteration < maxIterations && oracleCalls < maxOracleCalls) {
-      final double f = oracle.func(x);
-      oracle.grad(x, grad);
+      final double f = oracle.func(start);
+      oracle.grad(start, grad);
       oracleCalls++;
 
       final double gradNorm = grad.l2Norm();
@@ -67,36 +63,22 @@ public class ArmijoGD implements FirstOrderMinimizer {
       grad.mult(-1.0 / gradNorm, d);
 
       // Armijo
+      alpha *= 2;
       final double dDiff = grad.dot(d);
 
-      x.plus(alpha, d, armijoAttempt);
+      start.plus(alpha, d, armijoAttempt);
       double armijoTest = oracle.func(armijoAttempt);
       oracleCalls++;
 
-      if (armijoTest <= f + c1 * alpha * dDiff) {
-        while (armijoTest <= f + c1 * alpha * dDiff) {
-          oracleCalls++;
-          alpha *= eta;
+      while (armijoTest > f + c1 * alpha * dDiff) {
+        oracleCalls++;
+        alpha /= 2;
 
-          x.plus(alpha, d, armijoAttempt);
-          armijoTest = oracle.func(armijoAttempt);
-        }
-        alpha /= eta;
-        x.plus(alpha, d, armijoAttempt);
-      } else {
-        while (armijoTest > f + c1 * alpha * dDiff) {
-          oracleCalls++;
-          alpha /= eta;
-
-          x.plus(alpha, d, armijoAttempt);
-          armijoTest = oracle.func(armijoAttempt);
-        }
+        start.plus(alpha, d, armijoAttempt);
+        armijoTest = oracle.func(armijoAttempt);
       }
 
-      final Vector tmp = armijoAttempt;
-      armijoAttempt = x;
-      x = tmp;
-
+      armijoAttempt.copy(start);
       iteration++;
     }
   }
